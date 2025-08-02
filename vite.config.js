465730b5f410c7a46b401f96e4adab04e1941d6b
thiago valle <zthiagovalle@gmail.com>
2025-08-02 12:49:28 -0300

    chore: remove icon site

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
 });
